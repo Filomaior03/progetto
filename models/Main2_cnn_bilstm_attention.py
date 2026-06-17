0b4e6c77288ab64ef1e4fbe6d9f69927ae69f9e4
Filomaior03 <filomaior12@gmail.com>
2026-06-17 11:39:56 +0200

    Refactoring per verifica risultati caso studio

--- models/Main2_cnn_bilstm_attention.py
+++ models/Main2_cnn_bilstm_attention.py
@@ -1,15 +1,20 @@
 import os
 # os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 
-from keras.layers import Input, Dense, LSTM, merge ,Conv1D,Dropout,Bidirectional,Multiply,add,BatchNormalization
+from keras.layers import Input, Dense, LSTM ,Conv1D,Dropout,Bidirectional,Multiply,add,BatchNormalization
 from keras.models import Model
 import matplotlib.pyplot as plt
 
-from attention_utils import get_activations
-from keras.layers import merge
-from keras.layers.core import *
-from keras.layers.recurrent import LSTM
-from keras.models import *
+#librerie deprecate
+
+#from attention_utils import get_activations
+#from keras.layers import merge
+#from keras.layers.core import *
+#from keras.layers.recurrent import LSTM
+
+#from keras.models import *
+from keras.layers import Activation, Permute, Flatten, RepeatVector, Lambda
+
 import keras
 import  pandas as pd
 import  numpy as np
@@ -32,7 +37,9 @@ def attention_3d_block(inputs):
         a = RepeatVector(input_dim)(a)
     a_probs = Permute((1, 2), name='attention_vec')(a)
 
-    output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul')
+    #output_attention_mul = merge([inputs, a_probs], name='attention_mul', mode='mul')
+
+    output_attention_mul = Multiply()([inputs, a_probs])
     return output_attention_mul
 
 # Another way to write the attention mechanism Suitable for use with the above error reporting Source:https://blog.csdn.net/uhauha2929/article/details/80733255
@@ -67,8 +74,12 @@ def create_dataset(dataset, look_back):
         for i in range(k*153+look_back,(k+1)*153+1):
             a = dataset[(i-look_back):i,:]
             dataX.append(a)
-            dataY.append(dataset[i-1,6])
-    TrainX = np.array(dataX)[:,:,:6]
+
+            #dataset modificato
+            #dataY.append(dataset[i-1,6])
+            dataY.append(dataset[i-1,8])  #--> colonna target
+    #TrainX = np.array(dataX)[:,:,:6]
+    TrainX = np.array(dataX)[:,:,:8]
     Train_Y = np.array(dataY)
 
     return TrainX, Train_Y
@@ -84,7 +95,7 @@ def NormalizeMult(data):
     for i in range(0,data.shape[1]):
         #Column i
         list = data[:,i]
-        listlow,listhigh =  np.percentile(list, [0, 100])
+        listlow,listhigh = np.percentile(list, [0, 100])
         # print(i)
         normalize[i,0] = listlow
         normalize[i,1] = listhigh
@@ -135,21 +146,34 @@ def attention_model():
 
 #Load data
 
-data = pd.read_excel(". /New Microsoft Excel Worksheet.xlsx")
+data = pd.read_excel("data/data-ready-1.xlsx")
 
-data = data.drop(['Date (UTC)','Mean air temperature 2m(°C)','ET0','Water demand','Surface air pressure (hPa)'], axis = 1)
+#data = data.drop(['Date (UTC)','Mean air temperature 2m(°C)','ET0','Water demand','Surface air pressure (hPa)'], axis = 1)
 
-print(data.columns)
-print(data.shape)
+#nomi colonne codificati male, utilizzo indici per selezionare le colonne
+#tolgo queste colonne perché non sono utili alla previsione, inoltre mantengo le colonne che tengono conto del meteo, più importanti per la previsione
+#probabilmente le altre due colonne sono state tolte dagli autori perché, grazie alla GCA, sono risultate poco rilevanti
+data = data.drop(data.columns[[0, 9, 10]], axis = 1)
+
+# print("--- VERIFICA COLONNE DOPO IL DROP ---")
+# for idx, col in enumerate(data.columns):
+#     print(f"Indice {idx}: {col}")
+# print("-------------------------------------")
+
+# print(data.columns)
+# print(data.shape)
 
 
-INPUT_DIMS = 6
+#INPUT_DIMS = 6
+#gli input diventano 8 (tolgo 3 colonne invece di 5)
+INPUT_DIMS = 8
+
 TIME_STEPS = 20
 lstm_units = 64
 
 #normalize
 data,normalize = NormalizeMult(data)
-pollution_data = data[:,6].reshape(len(data),1)
+pollution_data = data[:,8].reshape(len(data),1)
 
 # train_X, _ = create_dataset(data,TIME_STEPS)
 # _ , train_Y = create_dataset(pollution_data,TIME_STEPS)
@@ -158,7 +182,7 @@ print(train_X.shape,train_Y.shape)
 
 m = attention_model()
 m.summary()
-m.compile(optimizer=keras.optimizers.Adam(lr=0.001), loss='mse')
+m.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001), loss='mse')
 
 history = m.fit(train_X[:21*134], train_Y[:21*134], epochs=500
                 , batch_size=512)
@@ -168,8 +192,15 @@ train_mse = mean_squared_error(result1,train_Y[:21*134])
 test_mse = mean_squared_error(result2,train_Y[21*134:])
 train_rmse = sqrt(mean_squared_error(result1,train_Y[:21*134]))
 test_rmse = sqrt(mean_squared_error(result2,train_Y[21*134:]))
-train_r2 = r2_score(result1,train_Y[:21*134])
-test_r2 = r2_score(result2,train_Y[21*134:])
+
+#BUG NEL CALCOLO DI R2, SONO INVERTITI I VALORI REALI (train_Y...) CON QUELLI PREVISTI (result1...), IL VALORE DI R2 NEL RESULT.TXT RISULTA ENORMEMENTE NEGATIVO
+
+# train_r2 = r2_score(result1,train_Y[:21*134])
+# test_r2 = r2_score(result2,train_Y[21*134:])
+
+train_r2 = r2_score(train_Y[:21*134], result1)
+test_r2 = r2_score(train_Y[21*134:], result2)
+
 train_mae = mean_absolute_error(result1,train_Y[:21*134])
 test_mae = mean_absolute_error(result2,train_Y[21*134:])
 #m.save("./model.h5")
